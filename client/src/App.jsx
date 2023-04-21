f9c88bcf547cc24349ae7dbf240fa201ef03357b
samthatcode <samthatcode@gmail.com>
2023-04-21 02:20:09 +0100

    Fixed bugs in the ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <div className="flex flex-col items-center w-full">
-        <div className="rounded-lg border-2 border-wine-900 text-blue-800 font-bold p-4 mb-6 mt-8 w-11/12 md:w-1/3">
+        <div className="rounded-lg border-2 border-wine-900 text-blue-700 font-bold p-4 mb-6 mt-8 w-11/12 md:w-1/3">
           <h4 className="text-2xl text-center">Video Chat App</h4>
         </div>
         <VideoPlayer />
