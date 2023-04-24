25605e4f7826cd56d285fa68ff226bdc6b45fb93
samthatcode <samthatcode@gmail.com>
2023-04-24 10:30:47 +0100

    Fixed a bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CreateRoom from "./routes/CreateRoom";
 import Room from "./routes/Room";
@@ -6,22 +6,7 @@ import Room from "./routes/Room";
 
 function App() {
   
-
-  useEffect(() => {
-    (function showMessage() {
-      setTimeout(function () {
-        setShowNotification(true);
-        setNotificationMessage('Dear Potential Employer, I would like to bring to your attention that due to technical issues with the host server, a certain feature of the project can only be viewed on localhost. I apologize for any inconvenience this may cause, but I assure you that the feature works perfectly on localhost. Thank you for your understanding.');
-        setTimeout(function () {
-          setShowNotification(false);
-          showMessage();
-        }, 5000);
-      }, 5000);
-    })();
-  }, []);
-
-  return (
-    <>
+  return (    
      
       <BrowserRouter>
         <Switch>
@@ -29,7 +14,7 @@ function App() {
           <Route path="/room/:roomID" component={Room} />
         </Switch>
       </BrowserRouter>
-   </>
+   
   );
 }
 
--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -22,8 +22,8 @@ const CreateRoom = (props) => {
                 setNotificationMessage(<p style={{ fontSize: '8px', margin: '0' }}>Dear Potential Employer, I would like to bring to your attention that due to technical issues with the host server, a certain feature of the project which is when users joins the room can only be viewed on localhost. I apologize for any inconvenience this may cause, but I assure you that the feature works perfectly on localhost. Thank you for your understanding.</p>);
                 setTimeout(function () {
                     setShowNotification(false);
-                    showMessage();
-                }, 5000);
+                }, 10000);
+                showMessage();
             }, 5000);
         })();
     }, []);
@@ -36,7 +36,7 @@ const CreateRoom = (props) => {
 
     return (
         <>
-            {showNotification && <Notification message={notificationMessage} />}
+            {showNotification && <Notification message={notificationMessage} className="show" />}
             <div className='welcome-div'>
                 <h4>Welcome! Click the button to Create Room</h4>
                 <button onClick={create}>Create room</button>
