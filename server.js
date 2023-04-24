7694dc2d3740f188924b41719ed684502008710d
samthatcode <samthatcode@gmail.com>
2023-04-24 08:10:50 +0100

    Fixed a bug

--- server.js
+++ server.js
@@ -8,53 +8,122 @@ const io = socket(server);
 const cors = require("cors");
 
 app.use(cors());
-const users = {};
 
+const users = {};
 const socketToRoom = {};
 
-io.on('connection', socket => {
-    socket.on("join room", roomID => {
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit("room full");
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
-        }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+app.get('/room/:id', (req, res) => {
+  // Retrieve the room ID from the URL parameter
+  const roomId = req.params.id;
 
-        socket.emit("all users", usersInThisRoom);
+  // Join the room using socket.io
+  io.on('connection', socket => {
+    socket.on("join room", roomID => {
+      if (users[roomID]) {
+        const length = users[roomID].length;
+        if (length === 4) {
+          socket.emit("room full");
+          return;
+        }
+        users[roomID].push(socket.id);
+      } else {
+        users[roomID] = [socket.id];
+      }
+
+      socketToRoom[socket.id] = roomID;
+      const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+      socket.emit("all users", usersInThisRoom);
     });
 
     socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+      io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
     });
 
     socket.on("returning signal", payload => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+      io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
     });
 
-
     socket.on('disconnect', () => {
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter(id => id !== socket.id);
-            users[roomID] = room;
-        }
+      const roomID = socketToRoom[socket.id];
+      let room = users[roomID];
+      if (room) {
+        room = room.filter(id => id !== socket.id);
+        users[roomID] = room;
+      }
     });
 
     socket.on('message', (msg) => {
-        console.log('Message Updated');
-        io.emit('message', msg);
+      console.log('Message Updated');
+      io.emit('message', msg);
     });
+  });
 
+  // Return a response
+  res.send(`Room ID: ${roomId}`);
 });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// require('dotenv').config();
+// const express = require("express");
+// const http = require("http");
+// const app = express();
+// const server = http.createServer(app);
+// const socket = require("socket.io");
+// const io = socket(server);
+// const cors = require("cors");
+
+// app.use(cors());
+// const users = {};
+
+// const socketToRoom = {};
+
+// io.on('connection', socket => {
+//     socket.on("join room", roomID => {
+//         if (users[roomID]) {
+//             const length = users[roomID].length;
+//             if (length === 4) {
+//                 socket.emit("room full");
+//                 return;
+//             }
+//             users[roomID].push(socket.id);
+//         } else {
+//             users[roomID] = [socket.id];
+//         }
+//         socketToRoom[socket.id] = roomID;
+//         const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+//         socket.emit("all users", usersInThisRoom);
+//     });
+
+//     socket.on("sending signal", payload => {
+//         io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+//     });
+
+//     socket.on("returning signal", payload => {
+//         io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+//     });
+
+
+//     socket.on('disconnect', () => {
+//         const roomID = socketToRoom[socket.id];
+//         let room = users[roomID];
+//         if (room) {
+//             room = room.filter(id => id !== socket.id);
+//             users[roomID] = room;
+//         }
+//     });
+
+//     socket.on('message', (msg) => {
+//         console.log('Message Updated');
+//         io.emit('message', msg);
+//     });
+
+// });
+
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
