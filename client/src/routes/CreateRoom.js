155b4710a726feeda35c1e4b3e16126d2210d718
samthatcode <samthatcode@gmail.com>
2023-04-24 10:58:00 +0100

    Fixed a bug

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -11,20 +11,20 @@ function Notification({ message }) {
 }
 
 const CreateRoom = (props) => {
-    const [showNotification, setShowNotification] = useState(false);
+    const [showNotification, setShowNotification] = useState(true);
     const [notificationMessage, setNotificationMessage] = useState('');
 
 
     useEffect(() => {
         (function showMessage() {
             setTimeout(function () {
-                setShowNotification(true);
+                setShowNotification(false);
                 setNotificationMessage(<p style={{ fontSize: '8px', margin: '0' }}>Dear Potential Employer, I would like to bring to your attention that due to technical issues with the host server, a certain feature of the project which is when users joins the room can only be viewed on localhost. I apologize for any inconvenience this may cause, but I assure you that the feature works perfectly on localhost. Thank you for your understanding.</p>);
                 setTimeout(function () {
-                    setShowNotification(false);
-                }, 10000);
-                showMessage();
-            }, 5000);
+                    setShowNotification(true);
+                    showMessage();
+                }, 4000);
+            }, 7000);
         })();
     }, []);
 
